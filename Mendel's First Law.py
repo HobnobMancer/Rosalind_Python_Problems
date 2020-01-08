1830a488e465fb5a3c43685914889e2463a7be14
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-01-08 14:18:13 +0000

    Add files via upload
    Add 'Counting Point Mutations.py', 'Enumerating Gene Orders.py', and 'Mendel's First Law.py'

---
+++ Mendel's First Law.py
@@ -0,0 +1,4 @@
+# http://rosalind.info/problems/iprb/
+# GIVEN: Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+# RETURN: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+
