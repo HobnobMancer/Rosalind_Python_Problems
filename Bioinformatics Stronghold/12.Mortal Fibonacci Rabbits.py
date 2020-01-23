54bbb7546d9ea1c2d39f700b3140c9bec7420464
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-01-23 16:56:46 +0000

    Create 12.Mortal Fibonacci Rabbits.py

---
+++ Bioinformatics Stronghold/12.Mortal Fibonacci Rabbits.py
@@ -0,0 +1,33 @@
+#Our aim is to somehow modify this recurrence relation to achieve a dynamic programming solution in the case that all rabbits die out after a fixed number of months. See Figure 4 for a depiction of a rabbit tree in which rabbits live for three months (meaning that they reproduce only twice before dying).
+
+#Given: Positive integers n≤100 and m≤20
+# Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for months.
+
+
+kits = 2 # number of kits per litter
+nth_month = 6
+lifespan = 3 # lifespan of rabbits
+
+
+# define function based on Fibonacci sequence to calcuate the total number of rabbit pairs in the nth month
+def rabbitFibonacci(n):
+    """Function to caclulate the number of rabbit paris in the nth month
+    Based upon the Fibonacci sequence
+    When calling the function 'n' represents the nth term for the sequence to terminate at"""
+
+    # 1 breading pairs in first month
+    if n==1:
+        return 1
+    # 1 breeding pairs in second month
+    elif n==2:
+        return 1
+    # breading pairs in subsequent months
+    elif lifespan < n:
+        (rabbitFibonacci(n-1) + (rabbitFibonacci(n-2)) * kits)
+    elif lifespan >= n:
+        return ((rabbitFibonacci(n-1)+(rabbitFibonacci(n-2))*kits)-rabbitFibonacci(n-lifespan))
+
+
+# call rabbit calculating function
+print("The number of rabbit pairs is", rabbitFibonacci(nth_month))
+
