4ca18425c4f682d8915991d9072557079eb527c7
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-02-10 15:11:50 +0000

    Add correct solution
    Update file with correct solution
    Move file to Bioinformatics stronghold folder

--- Bioinformatics Stronghold/6.Computing GC content.py
+++ Bioinformatics Stronghold/6.Computing GC content.py
@@ -1,5 +1,7 @@
-# The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
-# DNA strings must be labeled when they are consolidated into a database. A commonly used method of string labeling is called FASTA format. In this format, the string is introduced by a line that begins with '>', followed by some labeling information. Subsequent lines contain the string itself; the first line to begin with '>' indicates the label of the next string.
+# The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'.
+# For example, the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
+# DNA strings must be labeled when they are consolidated into a database.
+# A commonly used method of string labeling is called FASTA format. In this format, the string is introduced by a line that begins with '>', followed by some labeling information. Subsequent lines contain the string itself; the first line to begin with '>' indicates the label of the next string.
 # In Rosalind's implementation, a string in FASTA format will be labeled by the ID "Rosalind_xxxx", where "xxxx" denotes a four-digit code between 0000 and 9999.
 # Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 # Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
@@ -25,8 +27,9 @@ def GCcontentCalculator(string):
                 nucleotideCount[nucleotide] += 1
 
     # Calculate GC content
+    print(nucleotideCount)
     GCcontent = ((nucleotideCount['C'] + nucleotideCount['G']) / (len(string)) * 100)
-    return int(GCcontent)
+    return float(GCcontent)
 
 
 
@@ -36,7 +39,7 @@ def GCcontentCalculator(string):
 fastaFILE = open(r"/home/em/PycharmProjects/Rosalind/Bioinformatics Stronghold/CG_content_testFile", "rt")
 dnaSeq = fastaFILE.readlines()
 fastaFILE.close()
-
+dataSet = list(dnaSeq)
 
 # Separate IDs and store with associated GC counts as percentage
 
@@ -45,45 +48,31 @@ currentID = ""              # current working Rosalind ID
 currentDNA = ""             # current working DNA sequence
 dictionaryResults = {}      # contains Rosalind IDs and associated GC contents
 
-# Create first entries into dictionary
-    # Create first Rosalind ID
-firstLine = dnaSeq[0]
-firstID = firstLine[1:]
-
-    # Create first DNA sequence, as string on a single line
-firstSeq = dnaSeq[1] + dnaSeq[2]
-firstSequence = firstSeq.replace('\n', '')
-
- # Add first DNA sequence and Rosalind ID to the dictionary
-dictionaryResults[GCcontentCalculator(firstSequence)] = firstID
-
-
 # While loop to separate and store Rosalind IDs
-while dnaSeq is True:
-    fileLine = dnaSeq.readlines()
-    if fileLine is ">Rosalind*":
-        if currentID is not "":
-            currentID = fileLine[1:]
-            dictionaryResults[GCcontentCalculator(currentDNA)] = currentID
-            currentDNA = ""
+for fileLine in dnaSeq:
+    if fileLine[0] is ">":
+        currentID = fileLine[1:].replace('\n','')
+        currentDNA = ""
     else:
         strngFileline = str(fileLine)
-        currentDNA = currentDNA + strngFileline
-        currentDNA = currentDNA.replace('\n', '')
-        print(currentDNA)
+        currentDNA = currentDNA + strngFileline[:-1]
+        if currentID not in dictionaryResults.values():
+            dictionaryResults[GCcontentCalculator(currentDNA)] = currentID
+        else:
+            for key, value in dict(dictionaryResults).items():
+                if value is currentID:
+                    del dictionaryResults[key]
+            dictionaryResults[GCcontentCalculator(currentDNA)] = currentID
 
-# Add last Rosalind-DNA sequence pair to dictionary
-dictionaryResults[GCcontentCalculator(currentDNA)] = currentID
 
-
-# Find highest GC content
+# Find highest GC content, create empty list to store GC contents percentages
 allGCcontents = []
 
-for GCcontent in dictionaryResults.items():
-    allGCcontents.append(GCcontent)
+for key in dictionaryResults.keys():
+    allGCcontents.append(key)
 
 allGCcontents.sort()
-print(allGCcontents)
+
 
 # Print RosalindID and corresponding GC content with highest GC content
 if allGCcontents[-1] in dictionaryResults:
