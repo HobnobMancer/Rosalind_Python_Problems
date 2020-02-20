2e08a38d1b7dd6c5408e5c5f702a7a0fcd4ff7fe
Emma Hobbs <57451458+HobnobMancer@users.noreply.github.com>
2020-02-20 15:16:20 +0000

    Add correct solution
    Code will only print out UniProt ID of codes that contain the N-Glycosylation motif

--- Bioinformatics Stronghold/In Progress/15.Finding a protein motif.py
+++ Bioinformatics Stronghold/In Progress/15.Finding a protein motif.py
@@ -14,15 +14,58 @@
 
 
 from urllib.request import urlopen
+from Bio import SeqIO
+import re
 
 
 
-# Prepare Data
+# Create function to find N-Glycosylation motif in a given protein sequence
+def findMotifLocus(sequence, id):
+    """Find the location of the first amino acid of the N-glycosylation motif N{P}[ST]{P}, when {P} means any amino
+    acid except P, and [ST] means either S or T.
+    Pass to the function the amino acid sequence of the protein of interest and the index number of the UniProt ID
+    in the list of IDs created in the main body of the script"""
 
-# Extract IDs from text file
+    # create search range, motif cannot extend beyond the end of the sequence
+    searchRange = len(sequence) - 4
+
+    # create empty list to store starting amino acid position of motif
+    positions = []
+
+    # pass through sequence looking for all occurences of mofit, including overlapping occurences, retuning the position
+    # of the first amino acid in the protein sequence at which the motif starts
+    for j in range(0, searchRange):
+        # first, check if S and not T in third position
+        if sequence[j] is 'N' and sequence[j+1] is not 'P' and sequence[j+2] is 'S' and sequence[j+3] is not 'P':
+            aminoAcidPosition_S = j + 1
+            positions.append(aminoAcidPosition_S)
+            j += 1
+        # second, check if T and not S in third position
+        elif sequence[j] is 'N' and sequence[j+1] is not 'P' and sequence[j+2] is 'T' and sequence[j+3] is not 'P':
+            aminoAcidPosition_T = j + 1
+            positions.append(aminoAcidPosition_T)
+            j += 1
+
+    motifPositions = str(positions)
+    motifPositions = motifPositions.replace(',', '')
+    motifPositions = motifPositions.replace('[', '')
+    motifPositions = motifPositions.replace(']', '')
+
+    if len(positions) != 0:
+        print(uniprotIDsLIST[id])
+        print(motifPositions)
+
+    return
+
+
+
+
+# Prepare the given data
+
+# Extract IDs from text file which must contain no empty lines at the end of the file
 # open file with read only permit, read line by line
 # Open file containing UniProt IDs, and assign IDs to a variable (uniprotIDs)
-fastaFILES = open(r"C:\Users\Emma\Documents\PhD\Computational\Python\Rosalind\Rosalind_Python_Problems\Bioinformatics Stronghold\UniProtIDs.txt", "rt")
+fastaFILES = open(r"/home/em/PycharmProjects/Rosalind/Bioinformatics Stronghold/UniProtIDs", "rt")
 uniprotIDs = fastaFILES.readlines()
 fastaFILES.close()
 
@@ -48,17 +91,21 @@ for i in range(len(uniprotIDsLIST)):
 
 
 
-# Find N-glycosylation motif in protein sequences
+
+# Find N-glycosylation motif in protein sequences and return answer
 # N-glycosylaion motif: N{P}[ST]{P}.
 
+# open the fasta file
+handle = open('seq_file.fasta', 'r')
 
-# i = 0
-# for i in fastaSeq
-#   if N:
-#       if i+1 is not P:
-#           if i+2 is S or T:
-#               if i+3 is not P:
-#                   add to dictionarty
-#                   dictionaryNAME['key'].append(i)
+# create count to work through list of UniProt IDs
+idCount = 0
 
-# print dictionary
+# create dictionary to store results, and facilitate only printing of sequences containing the N-Glycosylation motif
+motifPositionDictionary = {}
+
+for record in SeqIO.parse(handle, 'fasta'):
+    if idCount < len(uniprotIDsLIST):   # prevent against index out of range error when adding values to dictionary
+        sequence = str(record.seq)
+        findMotifLocus(sequence, idCount)
+    idCount += 1
